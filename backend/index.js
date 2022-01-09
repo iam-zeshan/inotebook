84b124668f4d9c9b7374d2fabefd9c7143d12a7b
Zeshan Haider <zeshan.haider.turi@gmail.com>
2022-01-09 16:37:15 -0700

    I have done that how to save data into data base...

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,24 @@
 const connectToMongo = require("./db");
 connectToMongo();
-const express = require('express')
-const app = express()
-const port = 3000
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Below line will help us to send req.body data. Hint (thunderStorm body).
+app.use(express.json())
+// Available Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
+
 
 app.get('/', (req, res) => {
-  res.send('Hello amazing World!')
-})
+  res.send('Hello amazing World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
 
 
 

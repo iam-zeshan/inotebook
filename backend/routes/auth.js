77cc5ec6c773157bfb5c0e633c8a25da7b8ced1e
Zeshan Haider <zeshan.haider.turi@gmail.com>
2022-01-11 12:56:55 -0700

    Now I staged all of them (files)

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,25 +3,35 @@ const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
 
-// Create a User using:,     POST "/api/auth/". Doesn't require Auth
-router.post('/', [
+// Create a User using POST "/api/auth/createuser". Doesn't require Auth and login
+router.post('/createuser', [
     body('name').isLength({ min: 3 }),
     body('email').isEmail(),
     body('password').isLength({ min: 3 }),
-
-], (req, res) => {
+], async (req, res) => {
+    // If there are errors, return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    }).then(user => res.json(user))
-    .catch(err => console.log(err))
-    res.json({error: "the Email had already registered!"})
-    // res.send(req.body);
+    try {
+        // Check whether the user with this Email exists already
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({ error: "Sorry a user with this email alread exists!" })
+        }
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        res.send({ "Result": "Data Saved!" });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Sorry, some Error occured");
+    }
 })
 
 module.exports = router
+
+// just done something for git

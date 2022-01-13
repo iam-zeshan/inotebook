3665cef1fb0182e40000c4d736d34427fa761a87
Zeshan Haider <zeshan.haider.turi@gmail.com>
2022-01-12 22:41:04 -0700

    Done User Authentication

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs/dist/bcrypt');
+var jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'fuckyouhacker$';
 
 // Create a User using POST "/api/auth/createuser". Doesn't require Auth and login
+// is below ko ye End point keh raha hein
 router.post('/createuser', [
     body('name').isLength({ min: 3 }),
     body('email').isEmail(),
@@ -20,18 +25,60 @@ router.post('/createuser', [
         if (user) {
             return res.status(400).json({ error: "Sorry a user with this email alread exists!" })
         }
+
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
-        })
-        res.send({ "Result": "Data Saved!" });
+            password: secPass,
+        });
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SECRET);
+
+        res.send({ authToken });
     } catch (error) {
         console.error(error.message);
-        res.status(500).send("Sorry, some Error occured");
+        res.status(500).send("Sorry, internal server Errors occured");
     }
 })
 
-module.exports = router
+// Authentication using POST "/api/auth/login". Doesn't require Auth and login
+router.post('/login', [
+    body('email', 'enter a valid email').isEmail(),
+    body('password', 'password cannot be blank').exists(),
+], async (req, res) => {
+    // If there are errors, return Bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({email: req.body.email});
+        if (!user) {
+            return res.status(400).json({ error: "Please try to log in with correct credentials" });
+        }
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if (!passwordCompare) {
+            return res.status(400).json({ error: "Please try to log in with correct credentials" });
+        }
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SECRET);
+        res.send({ authToken });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Sorry, internal server Errors occured!");
+    }
+})
 
-// just done something for git
+
+module.exports = router
